184b65e3618c779d4dd7e68bb92f3395164c0ee5
handongqingxue <1417140290@qq.com>
2019-12-12 14:12:31 +0800

    完善

--- src/main/java/com/cqgVipShare/controller/VipController.java
+++ src/main/java/com/cqgVipShare/controller/VipController.java
@@ -1,19 +1,28 @@
 package com.cqgVipShare.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cqgVipShare.entity.InputMessage;
 import com.cqgVipShare.entity.ShareHistoryRecord;
 import com.cqgVipShare.entity.ShareRecord;
 import com.cqgVipShare.entity.ShareVip;
@@ -23,11 +32,14 @@ import com.cqgVipShare.service.VipService;
 import com.cqgVipShare.util.JsonUtil;
 import com.cqgVipShare.util.PlanResult;
 import com.cqgVipShare.util.qrcode.Qrcode;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 @Controller
 @RequestMapping("/vip")
 public class VipController {
-	
+
+	String TAG = "VipController";
 	@Autowired
 	private VipService vipService;
 	
@@ -273,6 +285,124 @@ public class VipController {
 			json=JsonUtil.getJsonFromObject(plan);
 		}
 		return json;
+	}
+	
+	@RequestMapping(value="/enterMenu")
+	public void enterMenu(HttpServletRequest request,HttpServletResponse response) throws IOException {
+
+		System.out.println("in.....");
+		ServletInputStream in = null;
+		try {
+			in = request.getInputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// 将POST流转换为XStream对象
+		XStream xs = new XStream(new DomDriver());
+		// 将指定节点下的xml节点数据映射为对象
+		xs.alias("xml", InputMessage.class);
+		// 将流转换为字符串
+		StringBuilder xmlMsg = new StringBuilder();
+		byte[] b = new byte[4096];
+		try {
+			for (int n; (n = in.read(b)) != -1;) {
+				xmlMsg.append(new String(b, 0, n, "UTF-8"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("xmlMsg=" + xmlMsg.toString());
+		
+		if (StringUtils.isEmpty(xmlMsg.toString())) {
+			// 设置编码
+			request.setCharacterEncoding("utf-8");
+			response.setContentType("html/text;charset=utf-8");
+			response.setCharacterEncoding("utf-8");
+			// 获取输出流
+			PrintWriter printWriter = response.getWriter();
+
+			// 设置一个全局的token,开发者自己设置。api这样解释：Token可由开发者可以任意填写，
+			// 用作生成签名（该Token会和接口URL中包含的Token进行比对，从而验证安全性）
+			String token = "cqgvs";
+			// 根据api说明，获取上述四个参数
+			String signature = request.getParameter("signature");
+			String timestamp = request.getParameter("timestamp");
+			String nonce = request.getParameter("nonce");
+			String echostr = request.getParameter("echostr");
+			
+			System.out.println("signature======="+signature);
+			System.out.println("timestamp======="+timestamp);
+			System.out.println("nonce======="+nonce);
+			System.out.println("echostr======="+echostr);
+			
+			// // temp:临时打印，观看返回参数情况
+			// System.out.println(TAG + ":signature:" + signature +
+			// ",timestamp:"
+			// + timestamp + ",nonce:" + nonce + ",echostr:" + echostr);
+			// 根据api所说的“加密/校验流程”进行接入。共计三步
+
+			// 第一步:将token、timestamp、nonce三个参数进行字典序排序
+			String[] parms = new String[] { token, timestamp, nonce };// 将需要字典序排列的字符串放到数组中
+			Arrays.sort(parms);// 按照api要求进行字典序排序
+			// 第二步:将三个参数字符串拼接成一个字符串进行sha1加密
+			// 拼接字符串
+			String parmsString = "";// 注意，此处不能=null。
+			for (int i = 0; i < parms.length; i++) {
+				parmsString += parms[i];
+			}
+			// sha1加密
+			String mParms = null;// 加密后的结果
+			MessageDigest digest = null;
+			try {
+				digest = java.security.MessageDigest.getInstance("SHA");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			digest.update(parmsString.getBytes());
+			byte messageDigest[] = digest.digest();
+			// Create Hex String
+			StringBuffer hexString = new StringBuffer();
+			// 字节数组转换为 十六进制 数
+			for (int i = 0; i < messageDigest.length; i++) {
+				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+				if (shaHex.length() < 2) {
+					hexString.append(0);
+				}
+				hexString.append(shaHex);
+			}
+			mParms = hexString.toString();// 加密结果
+
+			/*
+			 * api要求： 若确认此次GET请求来自微信服务器，请原样返回echostr参数内容， 则接入生效， 成为开发者成功，否则接入失败。
+			 */
+			// 第三步： 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信接入成功。
+			System.out.println(TAG + ":" + mParms + "---->" + signature);
+			if (mParms.equals(signature)) {
+				// System.out.println(TAG + ":" + mParms + "---->" + signature);
+				printWriter.write(echostr);
+			} else {
+				// 接入失败,不用回写
+				System.out.println(TAG + "接入失败");
+			}
+		}
+		else{
+		
+			System.out.println("1111111111111");
+			// 将xml内容转换为InputMessage对象
+			InputMessage inputMsg = (InputMessage) xs.fromXML(xmlMsg.toString());
+			// 取得消息类型
+			// String msgType = inputMsg.getMsgType();
+			// 根据消息类型获取对应的消息内容
+			// 文本消息
+			System.out.println("开发者微信号：" + inputMsg.getToUserName());
+			System.out.println("发送方帐号：" + inputMsg.getFromUserName());
+			System.out.println("消息创建时间：" + inputMsg.getCreateTime());
+			System.out.println("消息内容：" + inputMsg.getContent());
+			System.out.println("消息Id：" + inputMsg.getMsgId());
+			System.out.println("key：" + inputMsg.getEventKey());
+			PrintWriter printWriter = response.getWriter();
+			printWriter.print("11111111");
+		}
 	}
 
 }
